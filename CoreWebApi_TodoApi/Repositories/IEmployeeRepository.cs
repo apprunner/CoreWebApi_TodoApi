1285239273f271055de9ea2243fc229f2cda5764
skymax <dotnetcool@gmail.com>
2020-01-07 20:18:50 +0800

    加入專案檔案。

---
+++ CoreWebApi_TodoApi/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebApi_TodoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreWebApi_TodoApi.Repositories
+{
+    public interface IEmployeeRepository<T> where T:class
+    {
+         IEnumerable<T> GetAllEmployees();
+    }
+}
